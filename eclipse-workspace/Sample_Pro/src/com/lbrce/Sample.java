a912c1de2257f725cc05e64746244d745e35ed87
Jos <jogendra.moka@gmail.com>
2024-10-24 09:46:18 +0530

    First-commit

---
+++ eclipse-workspace/Sample_Pro/src/com/lbrce/Sample.java
@@ -0,0 +1,9 @@
+package com.lbrce;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for the test purpose1");
+	}
+}
